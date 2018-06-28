f0cfc5810bf1ed65ab1e0020417be662dc05595e
Juanpa102397 <juanpa.torres.m9237@gmail.com>
2018-06-28 16:34:29 -0500

    subida2
    GPS en Tiempo Real

---
+++ js/acciones.js
@@ -0,0 +1,35 @@
+// JavaScript Document
+$(document).ready(function(e) {
+document.addEventListener("deviceready", onDeviceReady,false);	
+});
+
+function onDeviceReady() {
+ $('#posicion').on('click',function(){
+ 	getPosition ();
+});
+}
+
+function getPosition(){
+ var options={
+      enableHighAccuracy :true,      
+      maximumAge:3600000 
+ 	}
+
+var watchID=navigator.geolocation.getCurrentPosition(onSuccess,onError,options);
+function onSuccess(position){
+
+  alert('Latitude:'+position.coords.latitude+'\n'+
+  	  'Longitude:'+position.coords.longitude+'\n'+
+  	    'Altitude:'+position.coords.altitude+'\n'+
+  	    'Accuracy:'+position.coords.accuracy+'\n'+
+  	    'Timestamp:'+position.timestamp+'\n');
+
+};
+
+
+	
+function onError(error){
+	alert('code:'+ error.code +'\n'+'message:'+error.message+'\n');
+  }
+}
+
